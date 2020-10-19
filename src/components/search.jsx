b97509bb3b56111f48f0d6930dd62908a25b872c
harshthakur066 <harshthakur066@gmail.com>
2020-10-19 15:56:07 +0530

    file structured

---
+++ src/components/search.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Search, Grid, Header, Segment } from "semantic-ui-react";
+import "./search.css";
+
+const SearchBox = () => {
+  //   const [state, setstate] = useState("");
+  return <div>Search</div>;
+};
+
+export default SearchBox;
