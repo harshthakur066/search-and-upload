87e65d3fc15f868c2fde5ba10851e9a83fb34211
Soumajit Choudhuri <43232595+Soumajit2000@users.noreply.github.com>
2020-10-20 12:23:37 +0530

    added search code

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -3,7 +3,11 @@ import { Search, Grid, Header, Segment } from "semantic-ui-react";
 import "./search.css";
 
 const SearchBox = () => {
-  //   const [state, setstate] = useState("");
+  //   const [state, setstate] = useState("")
+  <form method="post">
+     <input type="text" class="textbox" placeholder="Search">
+     <input title="Search" value="" type="submit" class="button">
+  </form>;
   return <div>Search</div>;
 };
 
